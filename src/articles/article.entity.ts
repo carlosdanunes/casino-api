b82e753afe7bf4e4d7817bfb9062270a7d3c07c9
Konstantin300 <topaykink@mail.ru>
2022-08-26 16:19:44 +0600

    update article entity

--- src/articles/article.entity.ts
+++ src/articles/article.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('article')
 export class Article {
@@ -6,11 +12,26 @@ export class Article {
   id: string;
 
   @Column({ type: 'varchar', length: 50 })
-  label: string;
+  title: string;
 
-  @Column({ type: 'varchar', length: 50, unique: true })
+  @Column({ type: 'text' })
   text: string;
 
   @Column({ type: 'varchar', length: 1000 })
+  subtitle: string;
+
+  @Column({ type: 'varchar', length: 1000 })
   imageUrl: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Column({ type: 'int' })
+  likesCount: number;
+
+  @Column({ type: 'int' })
+  viewsCount: number;
 }
